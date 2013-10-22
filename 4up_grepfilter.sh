871a772be29438b8dd897361c96546731ef29858
Niels Søholm <niels@niels-t410-debian>
2013-10-22 13:56:06 +0200

    Added a shell-script with usage examples for the R-Scripts

---
+++ 4up_grepfilter.sh
@@ -0,0 +1,4 @@
+# 4up_grepfilter1.r
+/usr/bin/Rscript up_grepfilter1.r /home/niels/jmeter/loginresults/200_poll.out /home/niels/jmeter/newpoll.svg
+# 4up_grepfilter2.r
+/usr/bin/Rscript 4up_grepfilter2.r /home/niels/jmeter/loginresults/ /home/niels/jmeter/newpoll2.svg _poll.out
